4ca4b262822b7a22a1970dd969f4017741dca0a1
Benjamin <ben@thefoghill.com>
2022-06-27 19:59:43 -0400

    added more characters

--- src/characters.js
+++ src/characters.js
@@ -3,15 +3,61 @@ export const characters = [
     id: 1,
     name: "Bowser",
     image: "https://kuroganehammer.com/images/ultimate/character/bowser.png",
+    Ultimate: {
+        Self: "http://api.kuroganehammer.com/api/characters/name/Bowser?game=ultimate",
+        Moves: "http://api.kuroganehammer.com/api/characters/name/Bowser/moves?game=ultimate",
+        Movements: "http://api.kuroganehammer.com/api/characters/name/Bowser/movements?game=ultimate",
+        Attributes: "http://api.kuroganehammer.com/api/characters/name/Bowser/characterattributes?game=ultimate",
+        UniqueProperties: "http://api.kuroganehammer.com/api/characters/name/Bowser/uniqueproperties?game=ultimate"
+    }
   },
   {
     id: 2,
     name: "Falco",
     image: "https://kuroganehammer.com/images/ultimate/character/falco.png",
+    Ultimate: {
+        Self: "http://api.kuroganehammer.com/api/characters/name/Falco?game=ultimate",
+        Moves: "http://api.kuroganehammer.com/api/characters/name/Falco/moves?game=ultimate",
+        Movements: "http://api.kuroganehammer.com/api/characters/name/Falco/movements?game=ultimate",
+        Attributes: "http://api.kuroganehammer.com/api/characters/name/Falco/characterattributes?game=ultimate",
+        UniqueProperties: "http://api.kuroganehammer.com/api/characters/name/Falco/uniqueproperties?game=ultimate"
+    }
   },
-  {
+    {
     id: 3,
     name: "Fox",
     image: "https://kuroganehammer.com/images/ultimate/character/fox.png",
+    Ultimate: {
+        Self: "http://api.kuroganehammer.com/api/characters/name/Fox?game=ultimate",
+        Moves: "http://api.kuroganehammer.com/api/characters/name/Fox/moves?game=ultimate",
+        Movements: "http://api.kuroganehammer.com/api/characters/name/Fox/movements?game=ultimate",
+        Attributes: "http://api.kuroganehammer.com/api/characters/name/Fox/characterattributes?game=ultimate",
+        UniqueProperties: "http://api.kuroganehammer.com/api/characters/name/Fox/uniqueproperties?game=ultimate"
+    
+  }},
+  {
+    id: 4,
+    name: "Kirby",
+    image: "https://kuroganehammer.com/images/ultimate/character/kirby.png",
+    Ultimate: {
+        Self: "http://api.kuroganehammer.com/api/characters/name/Kirby?game=ultimate",
+        Moves: "http://api.kuroganehammer.com/api/characters/name/Kirby/moves?game=ultimate",
+        Movements: "http://api.kuroganehammer.com/api/characters/name/Kirby/movements?game=ultimate",
+        Attributes: "http://api.kuroganehammer.com/api/characters/name/Kirby/characterattributes?game=ultimate",
+        UniqueProperties: "http://api.kuroganehammer.com/api/characters/name/Kirby/uniqueproperties?game=ultimate"
+    }
+    
   },
+  {
+    id: 5,
+    name: "Marth",
+    image: "https://kuroganehammer.com/images/ultimate/character/marth.png",
+    Ultimate: {
+        Self: "http://api.kuroganehammer.com/api/characters/name/Marth?game=ultimate",
+        Moves: "http://api.kuroganehammer.com/api/characters/name/Marth/moves?game=ultimate",
+        Movements: "http://api.kuroganehammer.com/api/characters/name/Marth/movements?game=ultimate",
+        Attributes: "http://api.kuroganehammer.com/api/characters/name/Marth/characterattributes?game=ultimate",
+        UniqueProperties: "http://api.kuroganehammer.com/api/characters/name/Marth/uniqueproperties?game=ultimate"
+    }}
 ];
+
